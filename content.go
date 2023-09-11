7a6c0fd9db2f7dc8f028d9656665343997b212d1
Misir Jafarov <misir.ceferov@gmail.com>
2023-09-12 02:25:28 +0400

    Improved html conversion (possibly containing xss)

--- content.go
+++ content.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"html"
+	"strings"
 )
 
 type Content struct {
@@ -11,7 +11,47 @@ type Content struct {
 
 func CompileContent(markdown string) (content Content, err error) {
 	content.Markdown = markdown
-	content.Html = html.EscapeString(markdown)
+	content.Html = compile(markdown)
 	err = nil
 	return
+}
+
+var (
+	htmlEscapeMap = map[byte]string{
+		'&':  "&amp;",
+		'\'': "&#39;", // "&#39;" is shorter than "&apos;" and apos was not in HTML until HTML5.
+		'<':  "&lt;",
+		'>':  "&gt;",
+		'"':  "&#34;",
+	}
+)
+
+func compile(s string) string {
+	var sb strings.Builder
+
+	newLines := 0
+
+	for _, c := range []byte(s) {
+		if c == '\r' {
+			continue
+		}
+
+		if c == '\n' {
+			newLines++
+			if newLines <= 2 {
+				sb.WriteString("<br/>")
+			}
+			continue
+		} else {
+			newLines = 0
+		}
+
+		if rep, ok := htmlEscapeMap[c]; ok {
+			sb.WriteString(rep)
+		} else {
+			sb.WriteByte(c)
+		}
+	}
+
+	return sb.String()
 }
